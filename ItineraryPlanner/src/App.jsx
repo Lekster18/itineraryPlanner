6eba640b64626ee1db964600f0d9f2ee0d676ad6
Leks <lekee96@gmail.com>
2024-03-14 20:38:12 +0800

    merge

--- ItineraryPlanner/src/App.jsx
+++ ItineraryPlanner/src/App.jsx
@@ -1,17 +1,10 @@
 import React from "react";
-<<<<<<< HEAD
-=======
 import CivicDistrict from "./components/CivicDistrict";
->>>>>>> 88769d695e7405e42b54b64fb5e4052c42052cd0
 
 function App() {
   return (
     <div>
-<<<<<<< HEAD
-      <h2>GA SEI</h2>
-=======
       <CivicDistrict />
->>>>>>> 88769d695e7405e42b54b64fb5e4052c42052cd0
     </div>
   );
 }
