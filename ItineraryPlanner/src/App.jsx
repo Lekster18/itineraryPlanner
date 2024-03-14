96f42aebd121ca9a95e712f99bac49842f7a60e2
Leks <lekee96@gmail.com>
2024-03-15 02:30:24 +0800

    add fetch function for favourites

--- ItineraryPlanner/src/App.jsx
+++ ItineraryPlanner/src/App.jsx
@@ -3,12 +3,14 @@ import CivicDistrict from "./components/CivicDistrict";
 import Login from "./pages/Login";
 import Search from "./pages/Search";
 import Dropdown from "./components/Dropdown";
+import FavouriteList from "./pages/FavouriteList";
 
 function App() {
   return (
     <div>
       {/* <Dropdown></Dropdown> */}
-      <Search></Search>
+      <FavouriteList />
+      {/* <Search></Search> */}
       {/* <Login></Login> */}
       {/* <CivicDistrict /> */}
     </div>
