6c033d6174390a74dc810f255be7c5610ba1f871
Leks <lekee96@gmail.com>
2024-03-16 23:02:43 +0800

    update fetch to favourites

--- ItineraryPlanner/src/pages/FavouriteList.jsx
+++ ItineraryPlanner/src/pages/FavouriteList.jsx
@@ -1,33 +1,24 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const FavouriteList = () => {
+  const [data, setData] = useState({});
   useEffect(() => {
     const getData = async () => {
       try {
         const res = await fetch(
-          "https://api.airtable.com/v0/appK0n8UQ1jxmNCyv/Table%201",
+          "https://api.airtable.com/v0/appK0n8UQ1jxmNCyv/Table%201/",
           {
-            method: "POST",
+            method: "GET",
             headers: {
               Authorization: `Bearer ${import.meta.env.VITE_AIRTABLE_API_KEY}`,
-              "Content-Type": "application/json",
             },
-            body: JSON.stringify({
-              records: [
-                {
-                  fields: {
-                    uuid: "007af02e4ad98554aeb939103509b7c5d56",
-                    name: "1000000PM",
-                  },
-                },
-              ],
-            }),
           }
         );
 
         if (res.ok) {
           const data = await res.json();
           console.log(data);
+          setData(data);
         } else {
           console.error("Error fetching");
           console.log(res);
@@ -36,13 +27,12 @@ const FavouriteList = () => {
         console.error("An error occurred:" + error.message);
       }
     };
-
-    setData();
+    getData();
   }, []);
 
   return (
     <>
-      <h1>Testtt</h1>
+      <h1>Testing fetch get</h1>
     </>
   );
 };
